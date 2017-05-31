83c487be915352d14c6956bb0a8d2f4595d9ca64
Lukasz Ludwin <gorli@interia.pl>
2017-05-31 22:24:40 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+﻿function getTriangleArea(a, h) {
+	if (!(a > 0 && h > 0)) {
+		return "Nieprawidłowe dane";
+	}
+
+	return a*h/2;
+}
+
+console.log(getTriangleArea(5,10));
+var triangle1Area = getTriangleArea(1,2);
+var	triangle2Area = getTriangleArea(20,2);
+var	triangle3Area= getTriangleArea(-2,2);
+console.log("triangle1: " + triangle1Area +", triangle2: " +  triangle2Area + ", triangle3: " + triangle3Area);
